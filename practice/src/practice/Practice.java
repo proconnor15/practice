648b1a21eb935ffc3b7bc40caaccc5d924935663
BullfrogJR <oconnorjr@outlook.com>
2017-03-05 12:52:00 -0500

    Changed output
    Said my name is Bullfrog

--- practice/src/practice/Practice.java
+++ practice/src/practice/Practice.java
@@ -16,7 +16,7 @@ public class Practice {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Yolo");
+        System.out.println("My name is Bullfrog!");
     }
     
 }
